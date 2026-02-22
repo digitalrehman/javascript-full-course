7ef07ff05387bd2c4925009a86efbad27a51c087
digitalrehman <abdulrehmanarain1018@gmail.com>
2026-02-22 14:38:11 +0900

    initail commit

---
+++ class 1/script.js
@@ -0,0 +1,79 @@
+/* console.log("Hello, World! This is a simple JavaScript file.");
+alert("Welcome to JavaScript programming!");
+console.log(window);
+*/
+
+// variables
+
+// var < 2015
+// let > 2015
+// const > 2015
+
+// string  "" / ''
+// var JohnDoe = "My name is John Doe";
+// var username = 'JohnDoe';
+// var username = "Janny"
+// username = "how are you"
+// console.log("username", username);
+// console.log("username" + username);
+
+// let username = "JohnDoe";
+// let username2 = "Janny"
+// username2 = "how are you"
+// console.log(username2);
+
+// const username = "JohnDoe";
+// username = "JohnDoe"; // This line would cause an error because const variables cannot be reassigned
+// console.log(username);
+
+// var (create same name variable name and you can update it)
+// var username = "JohnDoe";
+// username = "JaneDoe"; // This is allowed with var
+// console.log(username);
+
+// let (create cnanot same name variable name and you can update it)
+// let username = "JohnDoe";
+// let username = "JaneDoe"; // This would cause an error because you cannot redeclare a variable with let
+// username = "JaneDoe"; // This is allowed with let
+// console.log(username);
+
+// const (create youcant same name variable name and you cannot update it)
+// const username = "JohnDoe";
+// const username = "JaneDoe"; // This would cause an error because you cannot redeclare a variable with const
+// username = "JaneDoe"; // This would also cause an error because you cannot reassign a const variable
+// console.log(username);
+
+// let father-name = "John Doe";
+// let father name = "John Doe";
+// let 1fathername = "John Doe";
+// let %fathername = "John Doe";
+
+// let father_name = "John Doe"; // This is a valid variable name
+// let fatherName = "John Doe"; // This is also a valid variable name
+// let father2 = "John Doe";
+// let $father = "John Doe"; // This is a valid variable name
+// console.log(father2);
+
+// Data Types
+// String, Number, Boolean, Null, Undefined, Symbol, BigInt, Object
+
+// let names = "John Doe"; // String
+// let age = 30; // Number
+// let age = 30.34; // Number
+// let isStudent = true; // Boolean
+// let value;
+// let value = null; // Null
+// let bigint = 1234567890123456789012345678901234567890n; // BigInt
+// let symbol1 = Symbol("ali"); // Symbol
+// let symbol2 = Symbol("ali"); // Symbol
+
+// let user = [123, "John Doe", true, null, undefined, 1234567890123456789012345678901234567890n, Symbol("ali")];
+// let user = {
+//     name: "John Doe",
+//     age: 30,
+//     isStudent: true,
+//     value: null,
+//     bigint: 1234567890123456789012345678901234567890n,
+//     symbol: Symbol("ali")
+// };
+// console.log(typeof user);
