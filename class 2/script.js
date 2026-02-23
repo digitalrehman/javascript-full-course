549a6e57dc47f792cf358bb2d4e49fca70287ccc
digitalrehman <abdulrehmanarain1018@gmail.com>
2026-02-23 19:35:22 +0500

    add initial HTML and JavaScript files with basic arithmetic and operator examples

---
+++ class 2/script.js
@@ -0,0 +1,51 @@
+// airthmatic operations (+ - * / %)
+// relational operators (> < >= <= == != === !==)
+// logical operators (&& || !)
+// assignment operators (+= -= *= /= %=)
+// unary operators (++ --)
+
+// let num1 = 25;
+// let num2 = 10;
+// let res = num1 + num2;
+// let res = num1 - num2;
+// let res = num1 * num2;
+// let res = num1 / num2;
+// let res = num1 % num2;
+
+// let num1 = '20';
+// let num2 = '10';
+// console.log(num1 + num2);
+// console.log(num1 - num2);
+// console.log(num1 * num2);
+// console.log(num1 / num2);
+// console.log(num1 % num2);
+// console.log(num1 + num2 - 20);
+
+// let num1 = "20";
+// let num2 = 20;
+// let res = num1 < num2
+// let res = num1 > num2
+// let res = num1 >= num2
+// let res = num1 <= num2
+// let res = num1 == num2;
+// let res = num1 === num2;
+// let res = num1 != num2;
+// let res = num1 !== num2;
+
+// let num1 = 10;
+// let num2 = 5;
+// let res = num1 > num2 && num1 == 0;
+// let res = num1 > num2 && num1 == 0 && num2 != 0;
+// let res = num1 > num2 || num1 == 0;
+// let res = !(num1 < num2);
+
+// let num1 = 10;
+// num1 += 50;
+// num1 -= 50;
+// console.log(num1);
+
+let num1 = 10;
+// let res = ++num1;
+// let res = --num1;
+console.log(res);
+
