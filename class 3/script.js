0dffaec01568b97baf443e8bbca2a9deb47981ca
digitalrehman <abdulrehmanarain1018@gmail.com>
2026-02-25 20:24:07 +0500

    add index.html and script.js with initial HTML structure and commented JavaScript examples

---
+++ class 3/script.js
@@ -0,0 +1,145 @@
+// if (true)
+// else (false)
+// else if
+// switch case
+
+// type casting
+// prompt
+
+// let username1 = Number(prompt("enter your name1"))
+// let username2 = Number(prompt("enter your name2"))
+// let username1 = +prompt("enter your name1")
+// let username2 = +prompt("enter your name2")
+
+// let age1 = String(40)
+// let age2 = String(40)
+
+// let age1 = Boolean("true")
+
+// let age = +prompt("enter your age");
+// if (age > 18) {
+//   console.log("you are elgible for vote");
+// } else {
+//   console.log("you are not elgible for vote");
+// }
+
+// let age = 30;
+// if (age == 10) {
+//   console.log("eqaul to 10");
+// } else if (age == 20) {
+//   console.log("eqaul to 20");
+// } else if (age == 30) {
+//   console.log("eqaul to 30");
+// } else if (age == 40) {
+//   console.log("eqaul to 40");
+// } else {
+//   console.log("you are wrong");
+// }
+
+// let age = Number(prompt("enter your age"));
+// if (age > 0 && age < 18) {
+//   console.log("you are children");
+// } else if (age >= 18 && age <= 60) {
+//   console.log("you are elegible for cnic");
+// } else if (age > 60) {
+//   console.log("you are retired");
+// } else {
+//   console.log("invalid age");
+// }
+
+// let age = -90;
+// if (isNaN(age)) {
+//   console.log("sstring is not allow");
+// } else {
+//   if (age > 0 && age < 18) {
+//     console.log("you are children");
+//   } else if (age >= 18 && age <= 60) {
+//     console.log("you are elegible for cnic");
+//   } else if (age > 60) {
+//     console.log("you are retired");
+//   } else {
+//     console.log("invalid age");
+//   }
+// }
+
+// let age = +prompt("enter your age");
+// if (typeof age == "string") {
+//   console.log("char not allow");
+// } else {
+//   if (age > 0 && age < 18) {
+//     console.log("you are children");
+//   } else if (age >= 18 && age <= 60) {
+//     console.log("you are elegible for cnic");
+//   } else if (age > 60) {
+//     console.log("you are retired");
+//   } else {
+//     console.log("invalid age");
+//   }
+// }
+
+// switch case
+
+// let light = prompt("enter your color");
+// switch (light) {
+//   case "red":
+//     console.log("your color is red");
+//     break;
+//   case "blue":
+//     console.log("your color is blue");
+//     break;
+//   case "yellow":
+//     console.log("your color is yellow");
+//     break;
+//   case "black":
+//     console.log("your color is black");
+//     break;
+//   case "orange":
+//     console.log("your color is orange");
+//     break;
+//   default:
+//     console.log("pleaes enter correct color");
+// }
+
+// even odd number
+// let value = +prompt("enter your number")
+// if(value % 2){
+//   console.log("your no is odd");
+// }else{
+//   console.log("your no is even");
+// }
+
+// if (" ") {
+//   console.log("this is true");
+// } else {
+//   console.log("this is false");
+// }
+
+// NaN
+// false
+// 0
+// ""
+// null
+// undefined
+
+// let num1 = prompt("enter your no 1");
+// let num2 = prompt("enter your no 2");
+// let num3 = prompt("enter your no 3");
+
+// if (num1 > num2 && num1 > num3) {
+//   console.log("greater no is ", num1);
+// } else if (num2 > num1 && num2 > num3) {
+//   console.log("greater no is ", num2);
+// } else if (num3 > num1 && num3 > num2) {
+//   console.log("greater no is ", num3);
+// } else {
+//   console.log("invalid value");
+// }
+
+// swap 
+// let num1 = 390
+// let num2 = 20
+
+// let productPrice = 100
+// console.log(num1);
+// console.log(num2);
+
