259228d8aafa98cc7b942e93caaca9cb476c5aca
digitalrehman <abdulrehmanarain1018@gmail.com>
2026-02-28 19:50:19 +0500

    Your current version of Antigravity is out of date. Please visit https://antigravity.google/download to download and install the latest version.

---
+++ class 5/script.js
@@ -0,0 +1,67 @@
+// for (let i = 1; i <= 3; i++) {
+//     for (let j = 1; j <= 3; j++) {
+//         console.log(j);
+//     }
+//     console.log(i)
+// }
+
+// for (let i = 4; i <= 6; i++) {
+//     for (let j = 7; j <= 9; j++) {
+//         console.log(i);
+//     }
+//     console.log(i)
+// }
+
+
+// for (let i = 4; i <= 6; i++) {
+//     for (let j = 7; j <= 9; j++) {
+//         for (let k = 9; k <= 11; k++) {
+//             console.log(k);
+//         }
+//         console.log(j);
+//     }
+//     console.log(i)
+// }
+
+
+// for (let i = 1; i <= 5; i++) {
+//     for (let j = i; j <= 5; j++) {
+//         document.write("*" + " ");
+//     }
+//     document.write("<br>");
+// }
+
+
+// for (let i = 5; i >= 1; i--) {
+//     for (let j = i; j <= 5; j++) {
+//         document.write(j + " ");
+//     }
+//     document.write("<br>");
+// }
+
+
+
+
+// let i = 1
+// while (i <= 5) {
+//     let j = 1
+//     while (j <= 5) {
+//         console.log(j)
+//         j++
+//     }
+//     console.log(i)
+//     i++
+// }
+
+// let i = 1
+// while (i <= 5) {
+//     let j = i
+//     while (j <= 5) {
+//         document.write("*" + " ")
+//         j++
+//     }
+//     document.write("<br>")
+//     i++
+// }
+
+
