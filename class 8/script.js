08dc3ee89b8ad07afdb937fc2e1c9d2021671f0a
digitalrehman <abdulrehmanarain1018@gmail.com>
2026-03-06 22:40:14 +0500

    string & string methods

---
+++ class 8/script.js
@@ -0,0 +1,68 @@
+// let text = `my youtube channel name is upgrade skill`
+// let text = 'my youtube channel name is upgrade skill'
+// let text = String('my youtube channel name is upgrade skill')
+// let text = new String('my youtube channel name is upgrade skill')
+
+
+// Index 
+// let res = text[7]
+
+// template literal 2015
+// let fname = "abdul"
+// let lname = "rehman"
+// console.log("my name is " + fname + " " + lname);
+// console.log(`my name is ${fname} ${lname}`);
+
+// let res = text.length
+// let res = text.at(1)
+// let res = text.at(-3)
+// let res = text.charAt(2)
+// let res = text.charCodeAt(1)
+// let res = "😊".codePointAt(0)
+// let res = text.codePointAt(0)
+// let res = text.includes("my")
+// let res = text.indexOf("e")
+// let res = text.lastIndexOf("e")
+// let res = text.indexOf("e", 17 )
+// let res = text.startsWith("my")
+// let res = text.endsWith("skill")
+// let res = text.search("y")
+// let res = text.search(/\d/)
+// let res = text.slice(0,20)
+// let res = text.slice(-5)
+// let res = text.slice(10, 5)
+// let res = text.substring(0, 10)
+// let res = text.substring(-5)
+// let res = text.substring(10, 5)
+// let res = text.replace("my","MY")
+// let res = text.replace(/\d/, 89)
+// let res = text.replaceAll("e", "E")
+// let res = text.toLowerCase()
+// let res = text.toUpperCase()
+// let res = text.toLocaleLowerCase("tr")
+// let res = text.trimStart()
+// let res = text.trimEnd()
+// let res = text.trim()
+// let res = text.replace(/\s+/g, " ").trim()
+// let text = "abdulrehman@gmail.com"
+// let res = text.repeat(4)
+// let res = text.concat(" ","my name is abdul rehman")
+// let res = text.padStart(3,0)
+// let res = text.padEnd(3,0)
+// let res = text.split()
+// let res = text.split("@")
+// let res = text.split("")
+// let res = text.split(" ")
+// let res = text.split(/\d/)
+// let res = String("ali khan")
+// console.log(res.valueOf());
+// console.log(res.toString());
+// console.log(String.fromCharCode(97));
+// console.log(String.fromCodePoint(128522));
+// let text = "my youtube channel name is upgrade1 skill"
+// let text = "Item1 Item2 Item3"
+// let res = text.match(/\d/)
+// let res = text.match(/\d/g)
+// let res = "a".localeCompare("g")
+console.log(res);
+
