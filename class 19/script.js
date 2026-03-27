50275b143216d73fb27930d8917310995ff89a97
digitalrehman <abdulrehmanarain1018@gmail.com>
2026-03-27 12:36:53 +0500

    Add initial HTML and JavaScript files with DOM manipulation examples and commented code

---
+++ class 19/script.js
@@ -0,0 +1,78 @@
+// let head1 = document.getElementById("head2")
+// let head1 = document.getElementsByClassName("bold")[0]
+// let head1 = document.getElementsByTagName("h1")[0]
+// let head1 = document.querySelectorAll("h1")[1]
+// let head1 = document.querySelector("h1")
+// let head1 = document.querySelector(".bold")
+// let head1 = document.querySelector("#head2")
+// let head1 = document.head
+// let head1 = document.body
+// let head1 = document.forms
+// let head1 = document.all
+
+
+
+// function changeView(){
+//     head.style.backgroundColor = 'red'
+//     head.style.color = 'white'
+//     head.style.padding = "20px"
+// }
+
+
+// let text = head.innerText
+// let text = document.body.innerHTML
+
+// head.innerText = "Hello' Rehman"
+// head.innerHTML = "<i>This is heading 2 but this is italic tag</i>"
+
+
+// let head = document.getElementById("head2")
+// let italic = document.createElement("i")
+// italic.textContent = "This is heading 2 but this is italic tag"
+// head.appendChild(italic)
+
+
+
+
+// let res = head.getAttribute("id")
+// let res = head.setAttribute("class","blue")
+// console.log(res);
+
+
+// let head = document.getElementById("head2")
+// function changeView() {
+//     if (head.classList.contains("red")) {
+//         head.classList.remove("red")
+//         head.classList.add("blue")
+//     } else {
+//         head.classList.remove("blue")
+//         head.classList.add("red")
+//     }
+// }
+// function changeView() {
+//     head.classList.toggle("blue")
+// }
+
+// classList
+// head.classList.add("blue","green","100")
+// head.classList.remove("red")
+// head.classList.forEach((item) => {
+//     console.log(item);
+// })
+// head.classList.contains("red")
+// head.classList.length
+
+// console.log(head.classList.length);
+// console.log(head.classList.replace("red","reds"));
+
+
+// let head = document.getElementById("head2")
+// let res = head.nextSibling.nextSibling
+// let res = head.nextElementSibling
+// let res = head.nextElementSibling.nextElementSibling.nextElementSibling
+// let res = head.previousElementSibling.previousElementSibling
+// let res = head.parentElement.parentElement
+// let res = head.parentElement.lastElementChild.previousElementSibling
+// let res = head.parentElement.lastElementChild.previousElementSibling.firstElementChild
+// let res = head.parentElement.lastElementChild.previousElementSibling.lastElementChild
+// head.parentElement.lastElementChild.previousElementSibling.lastElementChild.previousElementSibling.style.color = "red"
